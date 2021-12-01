aca41d40005455c76d7fe8105cf485d3d0ef38fb
Frédéric Collonval <fcollonval@gmail.com>
2021-12-01 11:17:35 +0100

    Clean pages branch

--- packages/components/.storybook/customTheme.js
+++
@@ -1,7 +0,0 @@
-import {create} from '@storybook/theming/create';
-
-export default create({
-	base: 'light',
-	brandTitle: 'Jupyter UI Toolkit',
-	brandUrl: 'https://github.com/jupyterlab-contrib/jupyter-ui-toolkit',
-});
--- packages/components/src/utilities/storybook/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Jupyter Development Team.
-// Copyright (c) Microsoft Corporation.
-// Distributed under the terms of the Modified BSD License.
-
-export * from './focusObserver';
-export * from './createCodiconIcon';
